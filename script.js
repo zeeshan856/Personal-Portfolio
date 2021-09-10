da2caa34aad6b6f638f4fd9a3956e12cc5d8d8a7
Zeeshan Ahmed Irshad Ahmed <zeeshan.a8698@gmail.com>
2021-09-10 19:03:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show")
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show")
+        }
+    });
+
+    //slide up script
+
+     $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+     });
+
+    /*Toggle menu navbar script*/
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    //typing Animation Sctipt
+    var typed = new Typed(".typing",{
+        strings: ["Developer","UI Designer","Programmer"],
+        typedSpeed:100,
+        backSpeed:60,
+        loop: true 
+    });
+    var typed = new Typed(".typing-2",{
+        strings: ["Developer","UI Designer","Programmer"],
+        typedSpeed:100,
+        backSpeed:60,
+        loop: true 
+    });
+
+});
